2d8ddb87b79292a0208aa36046a59f908c83ec78
Petter Rasmussen <petter.rasmussen@gmail.com>
2013-01-28 20:42:32 +0100

    Remove unused functions

--- util/util.go
+++ util/util.go
@@ -64,100 +64,11 @@ func JoinInt(numbers []int64, sep string) string {
     return res
 }
 
-func FileSizeFormat(bytes int64) string {
-    units := []string{"B", "KB", "MB", "GB", "TB", "PB"}
-
-    var i int
-    value := bytes
-
-    for value > 1000 {
-        value /= 1000
-        i++
-    }
-    return fmt.Sprintf("%d %s", value, units[i])
-}
-
-// Truncates string to given max length, and inserts ellipsis into
-// the middle of the string to signify that the string has been truncated
-func TruncateString(str string, maxRunes int) string {
-    indicator := "..."
-
-    // Number of runes in string
-    runeCount := utf8.RuneCountInString(str)
-
-    // Return input string if length of input string is less than max length
-    // Input string is also returned if max length is less than 9 which is the minmal supported length
-    if runeCount <= maxRunes || maxRunes < 9 {
-        return str
-    }
-    
-    // Number of remaining runes to be removed
-    remaining := (runeCount - maxRunes) + utf8.RuneCountInString(indicator)
-    
-    var truncated string
-    var skip bool
-    
-    for leftOffset, char := range str {
-        rightOffset := runeCount - (leftOffset + remaining)
-
-        // Start skipping chars when the left and right offsets are equal
-        // Or in the case where we wont be able to do an even split: when the left offset is larger than the right offset
-        if leftOffset == rightOffset || (leftOffset > rightOffset && !skip) {
-            skip = true
-            truncated += indicator
-        }
-
-        if skip && remaining > 0 {
-            // Skip char and decrement the remaining skip counter
-            remaining--
-            continue
-        }
-        
-        // Add char to result string
-        truncated += string(char)
-    }
-    
-    // Return truncated string
-    return truncated
-}
-
 func TimeToLocal(t time.Time) string {
     local := t.Local()
     year, month, day := local.Date()
     hour, min, sec := local.Clock()
     return fmt.Sprintf("%04d-%02d-%02d %02d:%02d:%02d", year, month, day, hour, min, sec)
-
-}
-
-func ISODateToLocal(iso string) string {
-    t, err := time.Parse(time.RFC3339, iso)
-    if err != nil {
-        return iso
-    }
-    return TimeToLocal(t)
-}
-
-func MeasureTransferRate() func(int64)string {
-    start := time.Now()
-
-    return func(bytes int64) string {
-        seconds := int64(time.Now().Sub(start).Seconds())
-        if seconds < 1 {
-            return fmt.Sprintf("%s/s", FileSizeFormat(bytes))
-        }
-        bps := bytes / seconds
-        return fmt.Sprintf("%s/s", FileSizeFormat(bps))
-    }
-}
-
-// Prints a map in the provided order with one key-value-pair per line
-func Print(m map[string]interface{}, keyOrder []string) {
-    for _, key := range keyOrder {
-        value, ok := m[key]
-        if ok && value != "" {
-            fmt.Printf("%s: %v\n", key, value)
-        }
-    }
 }
 
 // Prints a struct with one name-value-pair per line
